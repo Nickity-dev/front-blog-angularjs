03f8843ece1bf4bd1240d565e7967507f0a5dda4
Nick <nikolemaio1234@gmail.com>
2026-09-24 14:12:20 -0300

    Iniciando

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-angular.module('blog', []);
+angular.module('blog', []); //lista de modulos dentro do []
 angular.module('blog').controller('Rest', function ($scope, $http){
   $http.get('https://api-fake-blog.onrender.com/postagens').
       success(function(data) {
